d699d471b387f3d03096333a2a5f97e8fb93f565
thguss <thgus345@naver.com>
2023-02-18 16:33:59 +0900

    엔티티 클래스 개발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 public class Address {
 
