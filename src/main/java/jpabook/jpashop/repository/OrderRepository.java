6e48eb6e511c18e496081e0c741fa865062def8c
thguss <thgus345@naver.com>
2023-02-20 00:03:02 +0900

    주문 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+	private final EntityManager em;
+
+	public void save(Order order) {
+		em.persist(order);
+	}
+
+	public Order findOne(Long orderId) {
+		return em.find(Order.class, orderId);
+	}
+
+	public List<Order> findAll() {
+		return em.createQuery("select o from Order o", Order.class).getResultList();
+	}
+}
