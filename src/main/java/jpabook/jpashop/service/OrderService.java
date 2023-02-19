d7d52bc955a7aa2566a0ecd2493c1a7fbb70bddd
thguss <thgus345@naver.com>
2023-02-20 02:40:41 +0900

    주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -13,6 +13,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -63,7 +64,7 @@ public class OrderService {
 	/**
 	 * 주문 검색
 	 */
-	// public List<Order> findOrders(OrderSearch orderSearch) {
-	// 	return orderRepository.findAll();
-	// }
+	public List<Order> findOrders(OrderSearch orderSearch) {
+		return orderRepository.findAllByString(orderSearch);
+	}
 }
