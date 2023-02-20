d2a13b3896bcba979476f9d7425c7a9fa54f93c3
thguss <thgus345@naver.com>
2023-02-20 22:59:16 +0900

    상품 수정

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
@@ -44,5 +47,38 @@ public class ItemController {
 		List<Item> items = itemService.findItems();
 		model.addAttribute("items", items);
 		return "items/itemList";
+	}
+
+	@GetMapping("items/{itemId}/edit")
+	public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+		Book item = (Book)itemService.findOne(itemId);
+
+		BookForm form = new BookForm();
+		form.setId(item.getId());
+		form.setName(item.getName());
+		form.setPrice(item.getPrice());
+		form.setStockQuantity(item.getStockQuantity());
+		form.setIsbn(item.getIsbn());
+		form.setAuthor(item.getAuthor());
+
+		model.addAttribute("form", form);
+
+		return "items/updateItemForm";
+	}
+
+	@PostMapping("items/{itemId}/edit")
+	public String updateItem(@PathVariable("itemId") Long itemId, @ModelAttribute("form") BookForm form) {
+		Book book = Book.builder()
+			.id(form.getId())
+			.name(form.getName())
+			.price(form.getPrice())
+			.stockQuantity(form.getStockQuantity())
+			.isbn(form.getIsbn())
+			.author(form.getAuthor())
+			.build();
+
+		itemService.saveItem(book);
+
+		return "redirect:/items";
 	}
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.exception.NotEnoughtStockException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
