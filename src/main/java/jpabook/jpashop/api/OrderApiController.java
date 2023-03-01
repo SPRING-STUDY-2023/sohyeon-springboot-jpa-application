74cbd2d1c963b7d62a5a6949558c692849fbdb9e
thguss <thgus345@naver.com>
2023-03-02 05:15:22 +0900

    [Section4] 엔티티를 Dto로 변환

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,15 +1,19 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.OrderService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -29,5 +33,49 @@ public class OrderApiController {
 			items.forEach(item -> item.getItem().getName());
 		}
 		return all;
+	}
+
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> ordersV2() {
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+		return orders.stream()
+			.map(OrderDto::new)
+			.toList();
+	}
+
+	@Getter
+	static class OrderDto {
+		private final Long orderId;
+		private final String name;
+		private final LocalDateTime orderDate;
+		private final OrderStatus status;
+		private final Address address;
+		private final List<OrderItemDto> orderItems;
+
+		public OrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getUsername();
+			orderDate = order.getOrderDate();
+			status = order.getStatus();
+			address = order.getDelivery().getAddress();
+			orderItems = order.getOrderItems().stream()
+				.map(OrderItemDto::new)
+				.toList();
+		}
+	}
+
+	@Getter
+	static class OrderItemDto {
+		private final String itemName;
+		private final int orderPrice;
+		private final int count;
+
+		public OrderItemDto(OrderItem orderItem) {
+			itemName = orderItem.getItem().getName();
+			orderPrice = orderItem.getOrderPrice();
+			count = orderItem.getCount();
+		}
+
 	}
 }
