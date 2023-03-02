ae5fe23e6ff3924ee041d6afee4fe9a4ce3abcbb
thguss <thgus345@naver.com>
2023-03-02 10:55:08 +0900

    [Section4] 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Address;
@@ -47,6 +48,17 @@ public class OrderApiController {
 	@GetMapping("/api/v3/orders")
 	public List<OrderDto> ordersV3() {
 		List<Order> orders = orderRepository.findAllWithItem();
+
+		return orders.stream()
+			.map(OrderDto::new)
+			.toList();
+	}
+
+	@GetMapping("/api/v3.1/orders")
+	public List<OrderDto> ordersV3_page(
+		@RequestParam(value = "offset", defaultValue = "0") int offset,
+		@RequestParam(value = "limit", defaultValue = "100") int limit) {
+		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit) ;
 
 		return orders.stream()
 			.map(OrderDto::new)
